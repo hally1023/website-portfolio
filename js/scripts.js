19beed4a231b0e9f2f856b203428b36bb286c2ae
hally1023 <hally.sim@gmail.com>
2025-03-25 11:55:15 -0400

    update files to watermark, update resume with website, update temporary password

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     b2bLink.addEventListener("click", function (event) {
       event.preventDefault(); // Prevents the link from opening immediately
   
-      const correctPassword = "carrot1023"; // Temporary Password
+      const correctPassword = "carrot10"; // Temporary Password
       const userInput = prompt("Enter password to access the document:");
   
       if (userInput === correctPassword) {
         window.open(
-          "https://docs.google.com/document/d/1j5_nF6fEgoZD1iGiLp6G2sXav_X5mXER/preview",
+          "https://drive.google.com/file/d/1a5bulYRmjQn6OkVjM30piV37oWa_jhC2/view",
           "_blank"
         );
       } else {
